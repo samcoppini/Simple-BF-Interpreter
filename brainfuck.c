b1090709f72124275f4e89a5141c3d367a229bdb
samcoppini <samcoppini@users.noreply.github.com>
2016-04-23 02:15:35 -0700

    Pre-compute loop jumps instead of on the fly

--- brainfuck.c
+++ brainfuck.c
@@ -1,78 +1,116 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//Reads a file and returns a pointer to a string with that file's contents
-char *read_file(FILE *file) {
-	int length = 0, allocated = 1;
-	unsigned char *str = (unsigned char *) malloc(allocated);
-	while (!feof(file)) {
-		str[length++] = fgetc(file);
-		if (length == allocated) {
-			allocated *= 2;
-			str = (unsigned char *) realloc(str, allocated);
-		}
-	}
-	fclose(file);
-	str[length++] = '\0';
-	str = (unsigned char *) realloc(str, length);
-	return str;
-}
+enum CommandKind {
+	CMD_CHANGE,
+	CMD_MOVE,
+	CMD_LOOP_BEGIN,
+	CMD_LOOP_END,
+	CMD_INPUT,
+	CMD_OUTPUT
+};
 
-//Executes a given brainfuck program with the given input file
-void execute(char *code, FILE *input) {
-	char tape[30000] = {0};
-	int tape_pos, code_pos;
-	for (tape_pos = 0, code_pos = 0; code[code_pos] != '\0'; code_pos++) {
-		switch (code[code_pos]) {
-			case '+':
-				tape[tape_pos]++;
+typedef struct Command {
+	enum CommandKind type;
+	int change_val;
+} Command;
+
+typedef struct Commands {
+	Command *cmds;
+	int num_commands;
+} Commands;
+
+typedef struct Node {
+	int val;
+	struct Node *next;
+} Node;
+
+//Reads a file and returns the commands in the file
+Commands read_file(FILE *file) {
+	Commands commands = {(Command *) malloc(sizeof(Command)), 0};
+	Node *loop_stack = NULL;
+	int allocated = 1, c;
+	while (!feof(file)) {
+		switch (c = fgetc(file)) {
+			case '+': case '-':
+			case '>': case '<':
+				commands.cmds[commands.num_commands].type = (c == '+' || c == '-') ? CMD_CHANGE: CMD_MOVE;
+				commands.cmds[commands.num_commands].change_val = (c == '+' || c == '>') ? 1 : -1;
+				commands.num_commands++;
 				break;
 				
-			case '-':
-				tape[tape_pos]--;
+			case '[': {
+				Node *temp = loop_stack;
+				loop_stack = (Node *) malloc(sizeof(Node));
+				loop_stack->val = commands.num_commands;
+				loop_stack->next = temp;
+				commands.cmds[commands.num_commands].type = CMD_LOOP_BEGIN;
+				commands.num_commands++;
 				break;
+			}
 				
-			case '>':
-				tape_pos++;
+			case ']': {
+				int loop_loc = loop_stack->val;
+				loop_stack = loop_stack->next;
+				commands.cmds[commands.num_commands].type = CMD_LOOP_END;
+				commands.cmds[commands.num_commands].change_val = loop_loc;
+				commands.cmds[loop_loc].change_val = commands.num_commands;
+				commands.num_commands++;
 				break;
-				
-			case '<':
-				tape_pos--;
-				break;
-				
-			case '[':
-				if (!tape[tape_pos]) {
-					//Move forward to the matching ]
-					int bracket_level = 1;
-					while (bracket_level > 0) {
-						code_pos++;
-						if (code[code_pos] == '[')
-							bracket_level++;
-						else if (code[code_pos] == ']')
-							bracket_level--;
-					}
-				}
-				break;
-				
-			case ']':
-				if (tape[tape_pos]) {
-					//Move backward to the matching [
-					int bracket_level = 1;
-					while (bracket_level > 0) {
-						code_pos--;
-						if (code[code_pos] == ']')
-							bracket_level++;
-						else if (code[code_pos] == '[')
-							bracket_level--;
-					}
-				}
-				break;
+			}
 				
 			case '.':
-				putchar(tape[tape_pos]);
+				commands.cmds[commands.num_commands].type = CMD_OUTPUT;
+				commands.num_commands++;
 				break;
 				
 			case ',':
+				commands.cmds[commands.num_commands].type = CMD_INPUT;
+				commands.num_commands++;
+				break;
+		}
+		if (commands.num_commands == allocated) {
+			allocated *= 2;
+			commands.cmds = (Command *) realloc(commands.cmds, allocated * sizeof(Command));
+		}
+	}
+	fclose(file);
+	commands.cmds = (Command *) realloc(commands.cmds, commands.num_commands * sizeof(Command));
+	return commands;
+}
+
+//Executes a given brainfuck program with the given input file
+void execute(Commands commands, FILE *input) {
+	char tape[30000] = {0};
+	Command *code = commands.cmds;
+	int tape_pos, code_pos;
+	for (tape_pos = 0, code_pos = 0; code_pos < commands.num_commands; code_pos++) {
+		switch (code[code_pos].type) {
+			case CMD_CHANGE:
+				tape[tape_pos] += code[code_pos].change_val;
+				break;
+				
+			case CMD_MOVE:
+				tape_pos += code[code_pos].change_val;
+				break;
+				
+			case CMD_LOOP_BEGIN:
+				if (!tape[tape_pos]) {
+					code_pos = code[code_pos].change_val;
+				}
+				break;
+				
+			case CMD_LOOP_END:
+				if (tape[tape_pos]) {
+					code_pos = code[code_pos].change_val;
+				}
+				break;
+				
+			case CMD_OUTPUT:
+				putchar(tape[tape_pos]);
+				break;
+				
+			case CMD_INPUT:
 				if (!input) {
 					printf("Error, was unable to read input file\n");
 					return;
@@ -96,12 +134,12 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 	
-	char *code = read_file(code_file);
+	Commands code = read_file(code_file);	
 	FILE *input_file = argc > 2 ? fopen(argv[2], "r"): NULL;
 	execute(code, input_file);
 	
 	//Clean up everything
-	free(code);
+	free(code.cmds);
 	if (input_file)
 		fclose(input_file);
 
