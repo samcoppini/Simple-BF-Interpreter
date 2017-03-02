8198d0221a4ef36a3fe6f256c8aced6457842a54
samcoppini <samcoppini@users.noreply.github.com>
2017-03-02 10:37:05 -0800

    Rework interpreter
    After coming back to this after leaving it for a few months, I found quite a few things I wanted to change about the interpreter, and ending up more-or-less rewriting it. The main thing that was changed was that I added a few functions for manipulating the data structures that the interpreter uses, although quite a few miscellaneous changes are present.

--- brainfuck.c
+++ brainfuck.c
@@ -1,274 +1,336 @@
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-enum CommandKind {
-	CMD_CHANGE,
-	CMD_ADD_PRODUCT,
-	CMD_SET,
-	CMD_MOVE,
-	CMD_LOOP_BEGIN,
-	CMD_LOOP_END,
-	CMD_INPUT,
-	CMD_OUTPUT
-};
+typedef enum CommandType {
+  CMD_CHANGE,
+  CMD_ADD_PRODUCT,
+  CMD_SET,
+  CMD_MOVE,
+  CMD_LOOP_BEGIN,
+  CMD_LOOP_END,
+  CMD_INPUT,
+  CMD_OUTPUT
+} CommandType;
 
 typedef struct Command {
-	enum CommandKind type;
-	int change_val, offset;
+  enum CommandType type;
+  int offset, change_val;
 } Command;
 
-typedef struct Commands {
-	Command *cmds;
-	int num_commands;
-} Commands;
+/* A struct for keeping track of the BF commands */
+typedef struct CommandList {
+  Command *cmds;
+  int num_cmds, cmds_allocated;
+} CommandList;
 
-typedef struct Node {
-	int val;
-	struct Node *next;
-} Node;
+#define INITIAL_COMMANDS 32
 
-//Adds an end to a loop to a list of commands, optimizing it away if possible
-void add_loop_end(Commands *commands, int loop_start) {
-	//If the previous command is a loop ending, we don't need an extra one,
-	//so just make sure to adjust the loop's beginning
-	if (commands->cmds[commands->num_commands - 1].type == CMD_LOOP_END) {
-		commands->cmds[loop_start].change_val = commands->num_commands - 1;
+/* A stack for keeping track of loops in the BF code */
+typedef struct Stack {
+  int *elements;
+  int num_elements, num_allocated;
+} Stack;
+
+#define INITIAL_STACK_SIZE 16
+
+/* Returns a pointer to a newly-allocated stack */
+Stack *new_stack() {
+  Stack *stack = malloc(sizeof(Stack));
+  stack->elements = malloc(INITIAL_STACK_SIZE * sizeof(int));
+  stack->num_allocated = INITIAL_STACK_SIZE;
+  stack->num_elements = 0;
+  return stack;
+}
+
+/* Frees a stack */
+void free_stack(Stack *stack) {
+  free(stack->elements);
+  free(stack);
+}
+
+/* Returns whether a stack is empty */
+bool stack_empty(Stack *stack) {
+  return stack->num_elements == 0;
+}
+
+/* Pushes an integer to to stack, allocating more space if need be */
+void stack_push(Stack *stack, int val) {
+  if (stack->num_allocated == stack->num_elements) {
+    stack->num_allocated <<= 1;
+    stack->elements = realloc(stack->elements,
+                              stack->num_allocated * sizeof(int));
+  }
+  stack->elements[stack->num_elements++] = val;
+}
+
+/* Pops the stack, returning the popped value */
+int stack_pop(Stack *stack) {
+  return stack->elements[--stack->num_elements];
+}
+
+/* Returns a newly-allocated list of commands */
+CommandList *new_command_list() {
+  CommandList *list = malloc(sizeof(CommandList));
+  list->cmds = malloc(INITIAL_COMMANDS * sizeof(Command));
+  list->cmds_allocated = INITIAL_COMMANDS;
+  list->num_cmds = 0;
+  return list;
+}
+
+/* Frees a list of commands */
+void free_commands(CommandList *list) {
+  free(list->cmds);
+  free(list);
+}
+
+/* Adds a new command to the end of a command list */
+void add_command(CommandList *list, CommandType type, int offset, int val) {
+  if (list->cmds_allocated == list->num_cmds) {
+    list->cmds_allocated <<= 1;
+    list->cmds = realloc(list->cmds, list->cmds_allocated * sizeof(Command));
+  }
+  list->cmds[list->num_cmds].type = type;
+  list->cmds[list->num_cmds].offset = offset;
+  list->cmds[list->num_cmds].change_val = val;
+  list->num_cmds++;
+}
+
+/* Adds an end to a loop, optimizing it away if possible */
+void add_loop_end(CommandList *list, int loop_start) {
+  /* If the previous command is a loop end, we don't need an extra one,
+     so we just make sure to adjust the loop's beginning */
+	if (list->cmds[list->num_cmds - 1].type == CMD_LOOP_END) {
+		list->cmds[loop_start].change_val = list->num_cmds - 1;
 		return;
 	}
 
-	//Looks through the contents of a loop, and checks to see if it can
-	//be optimized, and it keeps track of the leftmost and rightmost cells
-	//travelled to relative to the starting position
-	int cur_cmd, max_lcell = 0, max_rcell = 0, cur_cell = 0;
-	for (cur_cmd = loop_start + 1; cur_cmd != commands->num_commands; cur_cmd++) {
-		if (commands->cmds[cur_cmd].type == CMD_MOVE) {
-			cur_cell += commands->cmds[cur_cmd].change_val;
-			if (cur_cell > max_rcell)
-				max_rcell = cur_cell;
-			else if (cur_cell < max_lcell)
-				max_lcell = cur_cell;
-		}
-		else if (commands->cmds[cur_cmd].type != CMD_CHANGE)
-		//If a loop has commands other than +->< it can't be turned into
-		//a series of multiplications, so break out of it
-			break;
-	}
+  /* Look through the contents of a loop, and check to see if they can be
+     optimized away to a series of multiplications. Keeps track of how far
+     we move relative to the starting position */
+  int cur_cmd, max_lcell = 0, max_rcell = 0, cur_cell = 0;
+  for (cur_cmd = loop_start + 1; cur_cmd < list->num_cmds; cur_cmd++) {
+    if (list->cmds[cur_cmd].type == CMD_MOVE) {
+      cur_cell += list->cmds[cur_cmd].change_val;
+      if (cur_cell > max_rcell)
+        max_rcell = cur_cell;
+      else if (cur_cell < max_lcell)
+        max_lcell = cur_cell;
+    }
+    /* If a loop contains commands other than + - > <, it can't be optimized
+       away, so we stop looking */
+    else if (list->cmds[cur_cmd].type != CMD_CHANGE)
+      break;
+  }
 
-	//If all the commands in the loop were +->< and we end on the same cell we
-	//entered the loop on, we are able to change it to a series of multiplications,
-	//so we do that
-	if (cur_cmd == commands->num_commands && cur_cell == 0) {
-		int num_cells = max_rcell - max_lcell + 1;
-		int cell_changes[num_cells];
-		for (cur_cell = 0; cur_cell < num_cells; cur_cell++) {
-			cell_changes[cur_cell] = 0;
-		}
-		cur_cell = -max_lcell;
-		for (cur_cmd = loop_start; cur_cmd != commands->num_commands; cur_cmd++) {
-			if (commands->cmds[cur_cmd].type == CMD_CHANGE) {
-				cell_changes[cur_cell] += commands->cmds[cur_cmd].change_val;
-			}
-			else if (commands->cmds[cur_cmd].type == CMD_MOVE) {
-				cur_cell += commands->cmds[cur_cmd].change_val;
-			}
-		}
-		//We can't transform the loop to a series of multiplications unless the
-		//change to the starting cell is exactly -1
-		if (cell_changes[-max_lcell] == -1) {
-			commands->num_commands = loop_start;
-			for (cur_cell = 0; cur_cell < num_cells; cur_cell++) {
-				//If the cell changes in the loop, add a multiplication for it
-				if (cell_changes[cur_cell] != 0 && cur_cell != -max_lcell) {
-					commands->cmds[commands->num_commands].type = CMD_ADD_PRODUCT;
-					commands->cmds[commands->num_commands].change_val = cell_changes[cur_cell];
-					commands->cmds[commands->num_commands].offset = cur_cell + max_lcell;
-					commands->num_commands++;
-				}
-			}
-			//After we've added the multiplications, set the starting cell to 0
-			commands->cmds[commands->num_commands].type = CMD_SET;
-			commands->cmds[commands->num_commands].change_val = 0;
-			commands->num_commands++;
-			return;
-		}
-	}
+  /* If we didn't encounter any commands other than + - > <, and we ended the
+     loop on the same cell we started on, we can (probably) optimize the loop,
+     so go through the loop to see what changes are made to each cell */
+  if (cur_cmd == list->num_cmds && cur_cell == 0) {
+    int num_cells = max_rcell - max_lcell + 1;
+    int cell_changes[num_cells];
+    for (cur_cell = 0; cur_cell < num_cells; cur_cell++) {
+      cell_changes[cur_cell] = 0;
+    }
+    cur_cell = -max_lcell;
+    for (cur_cmd = loop_start; cur_cmd < list->num_cmds; cur_cmd++) {
+      if (list->cmds[cur_cmd].type == CMD_CHANGE) {
+        cell_changes[cur_cell] += list->cmds[cur_cmd].change_val;
+      }
+      else if (list->cmds[cur_cmd].type == CMD_MOVE) {
+        cur_cell += list->cmds[cur_cmd].change_val;
+      }
+    }
 
-	//If we couldn't optimize the loop, just add a loop end command normally
-	commands->cmds[commands->num_commands].type = CMD_LOOP_END;
-	commands->cmds[commands->num_commands].change_val = loop_start;
-	commands->cmds[loop_start].change_val = commands->num_commands;
-	commands->num_commands++;
+    /* If the change to starting cell is exactly -1, we can get rid of the loop
+       and transform it to a series of multiplications */
+    if (cell_changes[-max_lcell] == -1) {
+      list->num_cmds = loop_start;
+      /* Go through the list of cells we visit in the loop. If they are changed
+         in the loop, we have to add a multiplication for it */
+      for (cur_cell = 0; cur_cell < num_cells; cur_cell++) {
+        if (cell_changes[cur_cell] != 0 && cur_cell != -max_lcell) {
+          add_command(list, CMD_ADD_PRODUCT, cur_cell + max_lcell,
+                      cell_changes[cur_cell]);
+        }
+      }
+
+      /* At the end of the loop, we must set the starting cell to 0 */
+      add_command(list, CMD_SET, 0, 0);
+      return;
+    }
+  }
+
+  /* If we were unable to optimize the loop, add the loop end normally, and
+     adjust the beginning of the loop so it points to the new loop ending */
+  list->cmds[loop_start].change_val = list->num_cmds;
+  add_command(list, CMD_LOOP_END, 0, loop_start);
 }
 
-//Reads a file and returns the commands in the file
-Commands read_file(FILE *file) {
-	Commands commands = {(Command *) malloc(sizeof(Command)), 0};
-	Node *loop_stack = NULL;
-	int allocated = 1, c;
-	while (!feof(file)) {
-		switch (c = fgetc(file)) {
-			case '+': case '-':
-			case '>': case '<': {
-				enum CommandKind type = (c == '+' || c == '-') ? CMD_CHANGE: CMD_MOVE;
-				int change_amt = (c == '+' || c == '>') ? 1 : -1;
-				if (commands.num_commands > 0) {
-					if (type == CMD_CHANGE && commands.cmds[commands.num_commands - 1].type == CMD_SET) {
-						commands.cmds[commands.num_commands - 1].change_val += change_amt;
-						break;
-					}
-					else if (commands.num_commands > 0 && commands.cmds[commands.num_commands - 1].type == type) {
-						commands.cmds[commands.num_commands - 1].change_val += change_amt;
-						if (commands.cmds[commands.num_commands - 1].change_val == 0) {
-							commands.num_commands--;
-						}
-						break;
-					}
-				}
-				commands.cmds[commands.num_commands].type = type;
-				commands.cmds[commands.num_commands].change_val = change_amt;
-				commands.num_commands++;
-				break;
-			}
+/* Reads a brainfuck file, returning a list of commands */
+CommandList *read_file(FILE *file) {
+  CommandList *list = new_command_list();
+  Stack *loop_stack = new_stack();
+  int c;
+  while ((c = fgetc(file)) != EOF) {
+    switch (c) {
+      case '-': case '+': case '<': case '>': {
+        CommandType type = (c == '+' || c == '-') ? CMD_CHANGE: CMD_MOVE;
+        int change_amt = (c == '>' || c == '+') ? 1: -1;
+        if (list->num_cmds > 0) {
+          Command *last_command = &list->cmds[list->num_cmds - 1];
+          /* Combine like commands with each other, getting rid of them if
+             they cancel each other out */
+          if (last_command->type == type) {
+            last_command->change_val += change_amt;
+            if (last_command->change_val == 0)
+              list->num_cmds--;
+            break;
+          }
+          /* If we encounter a + - command after a set, we can combine them */
+          else if (type == CMD_CHANGE && last_command->type == CMD_SET) {
+            last_command->change_val += change_amt;
+            break;
+          }
+        }
+        /* If we couldn't combine this command with another, add it normally */
+        add_command(list, type, 0, change_amt);
+      }
+      break;
 
-			case '[':
-				if (commands.num_commands == 0 || commands.cmds[commands.num_commands - 1].type == CMD_LOOP_END ||
-				   (commands.cmds[commands.num_commands - 1].type == CMD_SET && commands.cmds[commands.num_commands - 1].change_val == 0))
-				{
-					int bracket_level = 1, c;
-					while (bracket_level > 0 && !feof(file)) {
-						c = fgetc(file);
-						if (c == ']')
-							bracket_level--;
-						else if (c == '[')
-							bracket_level++;
-					}
-				}
-				else {
-					Node *temp = loop_stack;
-					loop_stack = (Node *) malloc(sizeof(Node));
-					loop_stack->val = commands.num_commands;
-					loop_stack->next = temp;
-					commands.cmds[commands.num_commands].type = CMD_LOOP_BEGIN;
-					commands.num_commands++;
-				}
-				break;
+      case '[':
+        /* If we know that this loop can never be entered (if it's before any
+           other command, or it immediately follows another loop), we remove
+           the contents of the loop entirely */
+        if (list->num_cmds == 0 ||
+            list->cmds[list->num_cmds - 1].type == CMD_LOOP_END ||
+            (list->cmds[list->num_cmds - 1].type == CMD_SET &&
+             list->cmds[list->num_cmds - 1].change_val == 0))
+        {
+          int loop_level = 1;
+          while ((c = fgetc(file)) != EOF && loop_level > 0) {
+            if (c == '[')
+              loop_level++;
+            else if (c == ']')
+              loop_level--;
+          }
+          if (loop_level > 0) {
+            fprintf(stderr, "Error! Unmatched '[' found in the code!\n");
+            exit(1);
+          }
+        }
+        /* If we couldn't remove the loop, we add the loop start to the command
+           list and push its location onto the loop stack so we can complete
+           the loop when we encounter the matching ] command*/
+        else {
+          add_command(list, CMD_LOOP_BEGIN, 0, 0);
+          stack_push(loop_stack, list->num_cmds - 1);
+        }
+        break;
 
-			case ']':
-				if (loop_stack == NULL) {
-					printf("Error! Unmatched ] found in code!");
-					free(commands.cmds);
-					exit(0);
-				}
-				else {
-					Node *top = loop_stack;
-					loop_stack = loop_stack->next;
-					add_loop_end(&commands, top->val);
-					free(top);
-				}
-				break;
+      case ']':
+        if (stack_empty(loop_stack)) {
+          fprintf(stderr, "Error! Unmatched ']' found in the code.\n");
+          exit(1);
+        }
+        else {
+          add_loop_end(list, stack_pop(loop_stack));
+        }
+        break;
 
-			case '.':
-				commands.cmds[commands.num_commands].type = CMD_OUTPUT;
-				commands.num_commands++;
-				break;
+      case '.':
+        add_command(list, CMD_OUTPUT, 0, 0);
+        break;
 
-			case ',':
-				commands.cmds[commands.num_commands].type = CMD_INPUT;
-				commands.num_commands++;
-				break;
-		}
-		if (commands.num_commands == allocated) {
-			allocated *= 2;
-			commands.cmds = (Command *) realloc(commands.cmds, allocated * sizeof(Command));
-		}
-	}
-	if (loop_stack != NULL) {
-		while (loop_stack != NULL) {
-			Node *temp = loop_stack;
-			loop_stack = loop_stack->next;
-			free(temp);
-		}
-		printf("Error! Unmatched [ found in the code!");
-		fclose(file);
-		free(commands.cmds);
-		exit(0);
-	}
-	fclose(file);
-	commands.cmds = (Command *) realloc(commands.cmds, commands.num_commands * sizeof(Command));
-	return commands;
+      case ',':
+        add_command(list, CMD_INPUT, 0, 0);
+        break;
+    }
+  }
+  if (!stack_empty(loop_stack)) {
+    fprintf(stderr, "Error! Unmatched ']' found in the code.\n");
+    exit(1);
+  }
+  fclose(file);
+  free_stack(loop_stack);
+  return list;
 }
 
-//Executes a given brainfuck program with the given input file
-void execute(Commands commands, FILE *input) {
-	char tape[30000] = {0};
-	Command *code = commands.cmds;
-	int tape_pos, code_pos;
-	for (tape_pos = 0, code_pos = 0; code_pos < commands.num_commands; code_pos++) {
-		switch (code[code_pos].type) {
-			case CMD_CHANGE:
-				tape[tape_pos] += code[code_pos].change_val;
-				break;
+/* Executes a given brainfuck program with a given input file */
+void execute(CommandList *list, FILE *input_file) {
+  char tape[30000] = {0};
+  Command *code = list->cmds;
+  int tape_pos, code_pos, c;
+  for (tape_pos = 0, code_pos = 0; code_pos < list->num_cmds; code_pos++) {
+    switch (code[code_pos].type) {
+      case CMD_CHANGE:
+        tape[tape_pos] += code[code_pos].change_val;
+        break;
 
-			case CMD_ADD_PRODUCT:
-				tape[tape_pos + code[code_pos].offset] += tape[tape_pos] * code[code_pos].change_val;
-				break;
+      case CMD_ADD_PRODUCT: {
+        int product = tape[tape_pos] * code[code_pos].change_val;
+        tape[tape_pos + code[code_pos].offset] += product;
+        break;
+      }
 
-			case CMD_SET:
-				tape[tape_pos] = code[code_pos].change_val;
-				break;
+      case CMD_SET:
+        tape[tape_pos] = code[code_pos].change_val;
+        break;
 
-			case CMD_MOVE:
-				tape_pos += code[code_pos].change_val;
-				break;
+      case CMD_MOVE:
+        tape_pos += code[code_pos].change_val;
+        break;
 
-			case CMD_LOOP_BEGIN:
-				if (!tape[tape_pos]) {
-					code_pos = code[code_pos].change_val;
-				}
-				break;
+      case CMD_LOOP_BEGIN:
+        if (tape[tape_pos] == 0) {
+          code_pos = code[code_pos].change_val;
+        }
+        break;
 
-			case CMD_LOOP_END:
-				if (tape[tape_pos]) {
-					code_pos = code[code_pos].change_val;
-				}
-				break;
+      case CMD_LOOP_END:
+        if (tape[tape_pos] != 0) {
+          code_pos = code[code_pos].change_val;
+        }
+        break;
 
-			case CMD_OUTPUT:
-				putchar(tape[tape_pos]);
-				break;
+      case CMD_OUTPUT:
+        putchar(tape[tape_pos]);
+        break;
 
-			case CMD_INPUT:
-				if (!input) {
-					printf("Error, was unable to read input file\n");
-					return;
-				}
-				else
-					tape[tape_pos] = feof(input) ? 0: fgetc(input);
-				break;
-		}
-	}
+      case CMD_INPUT:
+        if (!input_file) {
+          fprintf(stderr, "Error! No input file given!\n");
+          return;
+        }
+        c = fgetc(input_file);
+        tape[tape_pos] = (c == EOF ? 0: c);
+        break;
+    }
+  }
 }
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		printf("Error, no brainfuck program given.\n");
-		return 0;
-	}
+  if (argc < 2) {
+    fprintf(stderr, "Error! No BF file given!\n");
+    return 1;
+  }
 
-	FILE *code_file = fopen(argv[1], "r");
-	if (!code_file) {
-		printf("Error, unable to read the brainfuck program.\n");
-		return 0;
-	}
+  FILE *bf_file = fopen(argv[1], "r");
+  if (!bf_file) {
+    fprintf(stderr, "Unable to open '%s'.", argv[1]);
+    return 1;
+  }
 
-	Commands code = read_file(code_file);
-	FILE *input_file = argc > 2 ? fopen(argv[2], "r"): NULL;
-	execute(code, input_file);
+  FILE *input_file = NULL;
+  if (argc > 2) {
+    input_file = fopen(argv[2], "r");
+    if (!input_file) {
+      fprintf(stderr, "Error! Unable to open '%s'.\n", argv[2]);
+      return 1;
+    }
+  }
 
-	//Clean up everything
-	free(code.cmds);
-	if (input_file)
-		fclose(input_file);
-
-	return 0;
+  CommandList *commands = read_file(bf_file);
+  execute(commands, input_file);
+  return 0;
 }
